63886fffd8236d37ef7e661c100cc5e63bef7452
maria <guttabingimaria@gmail.com>
2021-10-04 21:01:15 +0300

    added fast.js file

---
+++ fast.js
@@ -0,0 +1,24 @@
+function details(name, birth_year, current_year)
+{
+    age= current_year - birth_year;
+    reply= name+"is"+age+"years old"
+    console.log(reply)
+    return
+}
+details("guttabingi", 1998,2021)
+details("angella", 1990,2021)
+details("sarah", 2000, 2021)
+details("milly", 1770, 2021)
+details("peter", 1999, 2021)
+details("benja", 1998, 2021)
+function sum(a,b){
+    x=a+b;
+    return x
+}
+
+function mean(sum, number){
+    hence=sum/number;
+    there= "the mean of"+ number+" numbers is"+ hence
+    console.log(there)
+    return
+}
